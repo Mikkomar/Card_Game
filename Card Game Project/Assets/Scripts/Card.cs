7cc8760bc77919026d886527b35ade27f6b64b91
Mikkomar <43349616+Mikkomar@users.noreply.github.com>
2019-02-19 17:38:44 +0200

    Created template classes

---
+++ Card Game Project/Assets/Scripts/Card.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    protected string cardName;
+    protected string flavorText;
+    protected Sprite cardImage;
+    protected int militaryPowerCost;
+    protected int culturePowerCost;
+    protected int technologyPowerCost;
+
+    protected string cardTypeID;
+    protected string cardCultureID;
+
+    public void setCardSize(int x, int y)
+    {
+        gameObject.transform.Find("Back_Background").transform.localScale = new Vector3(x, 0, y);
+        gameObject.transform.Find("Front_Background").transform.localScale = new Vector3(x, 0, y);
+    }
+
+    public void initializeCard()
+    {
+        gameObject.transform.Find("Front_Background").transform.Find("Front_Picture").GetComponent<Image>().sprite = cardImage;
+    }
+
+    #region Gets and Sets
+    public void setCardName(string s)
+    {
+        cardName = s;
+    }
+
+    public string getCardName()
+    {
+        return cardName;
+    }
+
+    public void setFlavorText(string s)
+    {
+        flavorText = s;
+    }
+
+    public string getFlavorText()
+    {
+        return flavorText;
+    }
+
+    public void setCardImage(Sprite i)
+    {
+        cardImage = i;
+    }
+
+    public Sprite getCardImage()
+    {
+        return cardImage;
+    }
+
+    public void setMilitaryPowerCost(int i)
+    {
+        militaryPowerCost = i;
+    }
+
+    public int getMilitaryPowerCost()
+    {
+        return militaryPowerCost;
+    }
+
+    public void setCulturePowerCost(int i)
+    {
+        culturePowerCost = i;
+    }
+
+    public int getCulturePowerCost()
+    {
+        return culturePowerCost;
+    }
+
+    public void setTechnologyPowerCost(int i)
+    {
+        technologyPowerCost = i;
+    }
+
+    public int getTechnologyPowerCost()
+    {
+        return technologyPowerCost;
+    }
+
+    public void setCardTypeID(string s)
+    {
+        cardTypeID = s;
+    }
+
+    public string getCardTypeID()
+    {
+        return cardTypeID;
+    }
+
+    public void setCardCultureID(string s)
+    {
+        cardCultureID = s;
+    }
+
+    public string getCardCultureID()
+    {
+        return cardCultureID;
+    }
+    #endregion
+}
---
+++ Card Game Project/Assets/Scripts/Deck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck
+{
+    public int maximumAmountOfCards;
+    private List<Card> cardList;
+
+    #region Gets and Sets
+    public List<Card> getCardList()
+    {
+        return cardList;
+    }
+
+    public void setCardList(List<Card> l)
+    {
+        cardList = l;
+    }
+    #endregion
+}
