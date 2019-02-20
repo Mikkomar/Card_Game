1ab1fe24323b3ac01d7fe24d66d268495882bf03
Mikkomar <43349616+Mikkomar@users.noreply.github.com>
2019-02-20 17:52:39 +0200

    Hand UI improvements

--- Card Game Project/Assets/Scripts/Hand.cs
+++ Card Game Project/Assets/Scripts/Hand.cs
@@ -20,4 +20,29 @@ public class Hand : MonoBehaviour
             coordX = coordX + 50;
         }
     }
+
+    public void positionHand(Vector2 pos)
+    {
+        float coordX = pos.x/2;
+        float angle = 40;
+        float cardPosX;
+        float cardPosY;
+
+        /* Goes through both halves of hand and mirrorizes the card positions for symmetry and trigonometric calculations 
+         Using trigonometric functions to make the cards that are furthest away closer to the center than the cards already close to the center */
+        for(int i = 0; i <= hand.Count/2; i++)
+        {
+            /*  Position X: 30px from each other starting from both ends, minus the cosin value of the angle scaled by a constant
+                Position Y: 100px minus the sin value of the angle scaled by a constant */
+            cardPosX = 30 * (hand.Count / 2 - i) + (1 - Mathf.Abs(Mathf.Cos(angle * Mathf.PI / 180))) * 5;
+            cardPosY = Mathf.Abs(Mathf.Sin(angle * Mathf.PI / 180)) * 42;
+
+            hand[i].transform.position = new Vector2(coordX - cardPosX, 100 - cardPosY);
+            hand[i].transform.Rotate(0, 0, angle);
+            hand[hand.Count-1-i].transform.position = new Vector2(coordX + cardPosX, 100 - cardPosY);
+            hand[hand.Count-1-i].transform.Rotate(0, 0, -angle);
+            //coordX = coordX + 50;
+            angle = angle - 40 / (hand.Count / 2);
+        }
+    }
 }
